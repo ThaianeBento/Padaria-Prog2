1fd6dc3623125220abdf2001c6a6419ba9defcd2
ThaianeBento <thaianeba3112@gmail.com>
2024-09-23 09:39:53 -0300

    Spring Boot Initializr OneDrive - UDESC Universidade do Estado de Santa Catarina

---
+++ Java/DOO2/src/test/java/com/padaria/DOO2/Doo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padaria.DOO2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Doo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
