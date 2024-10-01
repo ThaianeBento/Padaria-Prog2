39a8e1c88dd0816357688b93075bc76aff6fa52d
Thaiane Bento <thaianeba3112@gmail.com>
2024-10-01 15:57:00 -0300

    Atendente DAO e diagrama de classes v1

--- Pads/src/main/java/Repository/AtendenteDAO.java
+++ Pads/src/main/java/Repository/AtendenteDAO.java
@@ -1,13 +1,82 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Repository;
 
-/**
- *
- * @author thaia
- */
+import Model.Atendente;
+import Model.Cliente;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.Persistence;
+
 public class AtendenteDAO {
+        private final EntityManagerFactory emf;
+        private final EntityManager em;
+        
+        AtendenteDAO(){
+            emf = Persistence.createEntityManagerFactory("Pads");
+            em = emf.createEntityManager();
+        }
+        
+    public void gravar(Atendente atendente){
+            try {
+            em.getTransaction().begin();
+            em.persist(atendente);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+    }
     
+    public Atendente remover(String CPF){
+        Atendente at = null;
+        
+        try {
+            em.getTransaction().begin();
+            at = (Atendente) em.createQuery("SELECT c FROM Atendente c WHERE c.CPF = :CPF", Atendente.class)
+                    .setParameter("CPF", CPF)
+                    .getSingleResult();
+            
+            if(at != null) {
+                em.remove(at);
+                em.getTransaction().commit();
+            }   
+            
+        } catch (NoResultException e) {
+        System.out.println("Atendente não encontrado com CPF: " + CPF);
+        em.getTransaction().rollback();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        
+        return at;
+    }
+
+public void atualizar(Atendente a) {
+        try{
+            em.getTransaction().begin();
+            em.merge(a);
+            em.getTransaction().commit();
+        }catch(Exception e){
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally{
+            closeConnection();
+        }
+    }
+        
+        
+    public void closeConnection(){ //método para fechar os EM e EMF
+        em.close();
+        emf.close();
+    }
+        
 }
+
+ 
+    
+
