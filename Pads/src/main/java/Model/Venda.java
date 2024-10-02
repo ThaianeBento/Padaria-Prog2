e28eabedb29696f8db424a5bbb02c1de9b4531e0
HenriqueKnaul <henrique.knaull@gmail.com>
2024-10-02 13:31:04 -0300

    Método de pagamento classe venda

--- Pads/src/main/java/Model/Venda.java
+++ Pads/src/main/java/Model/Venda.java
@@ -12,13 +12,14 @@ public class Venda {
     @GeneratedValue(strategy = GenerationType.AUTO)
     protected long id;
     private String cpfCliente;
+    private String metodoPagamento;
 
     // Relacionamento com produtos
     @ManyToMany
     @JoinTable(
-        name = "venda_produto", 
-        joinColumns = @JoinColumn(name = "venda_id"), 
-        inverseJoinColumns = @JoinColumn(name = "produto_id")
+            name = "venda_produto",
+            joinColumns = @JoinColumn(name = "venda_id"),
+            inverseJoinColumns = @JoinColumn(name = "produto_id")
     )
     private List<Produto> produtos;
 
@@ -26,7 +27,7 @@ public class Venda {
         this.cpfCliente = cpf;
         this.produtos = new ArrayList<>();
     }
-   
+
     public Venda() {
         this.produtos = new ArrayList<>();
     }
@@ -41,10 +42,26 @@ public class Venda {
         this.produtos.remove(p);
     }
 
+    public String escolhaPagamento (String metodoPagamento) {
+        switch (metodoPagamento){
+            case "1":
+                metodoPagamento = "Cartão de crédito";
+                break;
+            case "2":
+                metodoPagamento = "Cartão de débito";
+                break;
+            case "3":
+                metodoPagamento = "Dinheiro";
+                break;
+        }
+        return metodoPagamento;
+    }
+
     // Imprimir nota fiscal
     public void imprimirNotaFiscal() {
         System.out.println("Nota Fiscal da Venda:");
         System.out.println("Cliente (CPF): " + this.cpfCliente);
+        System.out.println("Método de pagamento: " + this.metodoPagamento);
         System.out.println("Produtos Comprados:");
         for (Produto p : this.produtos) {
             System.out.println(p.toString() + "\n");
