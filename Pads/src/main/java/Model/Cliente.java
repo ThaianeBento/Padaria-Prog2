cdcdcd0241b9a3ec1c26c64e273300d414eb22b0
Thaiane Bento <thaianeba3112@gmail.com>
2024-10-02 15:41:31 -0300

    Model Cliente

--- Pads/src/main/java/Model/Cliente.java
+++ Pads/src/main/java/Model/Cliente.java
@@ -1,47 +1,30 @@
 package Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 
-public class Cliente extends Usuario{
+@Entity
+public class Cliente extends Usuario {
 
+    @Column(nullable = false)
     private double pontos;
 
-    public Cliente(String cpf, String nome) {
+    public Cliente() {}
+
+     public Cliente(String cpf, String nome) {
         super(cpf, nome);
+        this.pontos = 0;  
     }
-
-    @Override
-    public String getCPF() {
-        return cpf;
-    }
-
-    @Override
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-
-    @Override
-    public String getNome() {
-        return nome;
-    }
-
-    @Override
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
 
     public double getPontos() {
         return pontos;
     }
 
-    public double addPontos(double valor){
-        return this.pontos += valor;
-    }
-
-    private void setPontos(double pontos) {
+    public void setPontos(double pontos) {
         this.pontos = pontos;
     }
 
-
+    public double addPontos(double valor) {
+        return this.pontos += valor;
+    }
 }
