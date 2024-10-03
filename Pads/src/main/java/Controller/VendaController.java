a442fd2ab80c85b3fd67c78c5fd8b33beae5e69e
mario <mariojralves2006@gmail.com>
2024-10-03 03:11:21 -0300

    venda controller

--- Pads/src/main/java/Controller/VendaController.java
+++ Pads/src/main/java/Controller/VendaController.java
@@ -1,2 +1,39 @@
-package Controller;public class VendaController {
+package Controller;
+
+import Model.Cliente;
+import Model.Produto;
+import Model.Venda;
+import Repository.VendaDAO;
+
+import java.util.List;
+
+public class VendaController {
+    VendaDAO vendaDAO;
+
+    public VendaController(){
+        vendaDAO = new VendaDAO();
+    }
+
+    public void create(List<Produto> produtos, Cliente cliente){
+        if(produtos.isEmpty()){
+            throw new IllegalArgumentException("Venda deve ter pelo menos um produto.");
+        }
+        if(cliente == null){
+            throw new IllegalArgumentException("Venda deve ter um cliente.");
+        }
+        vendaDAO.create(produtos, cliente);
+    }
+
+    public Venda buscarVendaPorId(long id){
+        return vendaDAO.buscarPorId(id);
+    }
+
+    public void remove(long id){
+        Venda venda = vendaDAO.buscarPorId(id);
+        if(venda != null){
+            vendaDAO.remove(id);
+        }
+    }
+
+
 }
